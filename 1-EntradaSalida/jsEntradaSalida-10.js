60b0399de5c21dc51252972eb59bff67dd706af6
Horacio Barrios <45366420+Orfhen9999@users.noreply.github.com>
2019-02-11 22:27:09 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,13 +4,45 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
-  var sueldo;
+
+       var precio;
+       var descuento;
+       var precioFinal;
+
+       precio=parseFloat(document.getElementById("importe").value);
+
+       descuento=precio * 25/100;
+
+       precioFinal=precio-descuento;
+
+       alert("el descuento efectuado es de $  "+descuento);
+
+       document.getElementById("resultado").value=precioFinal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*var sueldo;
   sueldo=parseInt(document.getElementById("importe").value);
   descuento=(sueldo*25)/100;
   resultado=(sueldo-descuento);
   document.getElementById("resultado").value=resultado;
 
-    
+    */
 
 
 
