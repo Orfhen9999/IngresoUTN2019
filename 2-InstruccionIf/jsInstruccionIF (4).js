e627722308d04d0731af8cdc2987723aa041b21b
Horacio Barrios <45366420+Orfhen9999@users.noreply.github.com>
2019-02-11 22:28:17 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,11 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+if(edad >=13 && edad <18){
+    alert("es adolecente   "+edad);
+}
 
 
 }//FIN DE LA FUNCIÓN
