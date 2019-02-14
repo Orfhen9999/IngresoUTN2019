67a8db9504baabb9dc0fa5807793d397daa97fb2
Horacio Barrios <45366420+Orfhen9999@users.noreply.github.com>
2019-02-13 22:19:35 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,11 +1,45 @@
 function Mostrar()
 {
 //tomo la edad  
+
+
+
+// no se entiende pero anda :D
+/*
 var edad;
+edad=parseInt(document.getElementById("edad").value);
+if(edad <13 != edad <=17){
+
+    alert("es adolecente");
+}
+
+
+*/
+
+
+
+
+
+
+//ejemplo de negacion: si edad NO es menor a 13 O edad mayor a 17 entonces es ADOLECENTE . 
+
+
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+if(!(edad <13 || edad >17)){
+
+    alert("es adolecente");
+
+}
+
+
+
+//ejemplo :si edad es mayor a 13 y edad menor a 18 es  VERDADERO (TRUE)
+/*var edad;
 edad=parseInt(document.getElementById("edad").value);
 if(edad >=13 && edad <18){
     alert("es adolecente   "+edad);
 }
-
+*/
 
 }//FIN DE LA FUNCIÓN
