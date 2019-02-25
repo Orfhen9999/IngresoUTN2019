0713279902d4fe96d605758802aab6949364fcdb
Horacio Barrios <45366420+Orfhen9999@users.noreply.github.com>
2019-02-25 02:39:40 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,52 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+var largo;
+var ancho;
+var perimetro;
+var alambre;
+// tomar los datos por document.getElementById. y calcular el perimetro.
+largo = parseInt(document.getElementById("Largo").value);
+ancho = parseInt(document.getElementById("Ancho").value);
+//perimetro es largo x 2 + ancho x 2;
+perimetro = largo * 2 +  ancho * 2;
+alambre = perimetro * 3;
+alert("la cantidad de alambre a usar es : "+ alambre);
 }
 function Circulo () 
 {
+var radio;
+var alambre;
+radio = parseInt(document.getElementById("Radio").value);
+//prerimetro es  2 x radio x pi
+perimetro = 2 * radio * Math.PI;
+alambre = perimetro * 3;
+alert("la cantidad de alambre a usar es : "+ alambre.toFixed(2));
+
+
+
+
+
+
 	
 }
 function Materiales () 
 {
-	
+    var largo;
+    var ancho;
+    var area;
+    var cemento;
+    var cal;
+    //AREA ES BASE POR ALTURA (LARGO X ANCHO)
+    largo = parseInt(document.getElementById("Largo").value);
+    ancho = parseInt(document.getElementById("Ancho").value);
+    area = largo * ancho;
+    cemento = 2 * area;
+    cal = 3 * area;
+    alert("la cant. de bolsas de cemento a usar es :   "+ cemento+"  la cantidad de cal a usar es : "+ cal );
+
+
+
+
+
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,55 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    var num1;
+    var num2;
+    var num3;
+    var suma;
+    num1 = parseInt(document.getElementById("PrecioUno").value);
+    num2 = parseInt(document.getElementById("PrecioDos").value);
+    num3 = parseInt(document.getElementById("PrecioTres").value);
+
+    suma = num1 + num2 + num3;
+
+    alert("la suma de los precios es $:  "+ suma);
+
+
+
 	
 }
 function Promedio () 
 {
-	
+    var num1;
+    var num2;
+    var num3;
+    var promedio;
+    var suma;
+    num1 = parseInt(document.getElementById("PrecioUno").value);
+    num2 = parseInt(document.getElementById("PrecioDos").value);
+    num3 = parseInt(document.getElementById("PrecioTres").value);
+
+    suma = num1 + num2 + num3;
+    promedio = suma / 5
+
+    alert("el promedio es  :  "+ promedio);
+    
 }
 function PrecioFinal () 
 {
-	
+	var num1;
+    var num2;
+    var num3;
+    var suma;
+    var iva;
+    var preciofinal;
+    num1 = parseInt(document.getElementById("PrecioUno").value);
+    num2 = parseInt(document.getElementById("PrecioDos").value);
+    num3 = parseInt(document.getElementById("PrecioTres").value);
+
+    suma = num1 + num2 + num3;
+    iva = (suma *21) /100;
+    preciofinal = suma + iva;
+    
+
+    alert("la suma de los precios es $:  "+ preciofinal);
 }
